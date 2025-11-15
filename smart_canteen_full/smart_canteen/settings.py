f7b01c8d6fbdb953f50501a9183d9ccab18ce6f2
matchanikhitha29 <nikhithamatcha29@gmail.com>
2025-11-15 15:06:51 +0530

    change3

--- smart_canteen_full/smart_canteen/settings.py
+++ smart_canteen_full/smart_canteen/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'demo-secret-key-change-in-production'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['smart-canteen-qozm.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
